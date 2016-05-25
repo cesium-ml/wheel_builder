5614b140719ba3955dd6feb400969cc0133265a2
Stefan van der Walt <stefanv@berkeley.edu>
2016-05-25 12:35:59 -0700

    Specify PYTHON_VERSIONS correctly for docker run

--- wheel_builder.sh
+++ wheel_builder.sh
@@ -5,5 +5,5 @@ if [[ "$TRAVIS_OS_NAME" == "osx" ]]; then
 else
     DOCKER_IMAGE=quay.io/pypa/manylinux1_x86_64
     docker pull $DOCKER_IMAGE
-    docker run --rm -e PYTHON_VERSIONS=$PYTHON_VERSIONS -v $PWD:/io $DOCKER_IMAGE /io/build_manylinux.sh
+    docker run --rm -e PYTHON_VERSIONS="$PYTHON_VERSIONS" -v $PWD:/io $DOCKER_IMAGE /io/build_manylinux.sh
 fi

94577afc8fc77e5233b5e63660db13302ada65c8
Stefan van der Walt <stefanv@berkeley.edu>
2016-05-25 15:59:48 -0700

    Refactor OSX build
    - Move OSX build into wheel_builder.sh
    - Remove usage of common_vars.sh in OSX, since it's designed to be used
      in the docker container

--- wheel_builder.sh
+++ wheel_builder.sh
@@ -2,11 +2,38 @@
 
 set -e
 
-curl -OL https://raw.githubusercontent.com/matthew-brett/manylinux-builds/master/common_vars.sh
+mkdir wheelhouse
+rm -rf wheelhouse/*
 
 if [[ "$TRAVIS_OS_NAME" == "osx" ]]; then
-    source ./build_osx.sh
+    WORK_DIR=$PWD
+
+    curl -OL https://raw.githubusercontent.com/MacPython/terryfy/master/travis_tools.sh
+    source travis_tools.sh
+
+    function cpython_path {
+        echo "$WORK_DIR/venv_$1"
+    }
+
+    mkdir -p unfixed_wheels
+    rm -rf unfixed_wheels/*
+
+    for PYTHON in ${PYTHON_VERSIONS}; do
+        get_python_environment macpython $PYTHON "$(cpython_path $PYTHON)"
+        pip install delocate numpy==$NUMPY_VERSION cython
+    done
+
+    source pip_build_wheels.sh
+
+    delocate-listdeps unfixed_wheels/*
+    delocate-wheel unfixed_wheels/*.whl
+    delocate-addplat --rm-orig -x 10_9 -x 10_10 dist/*.whl
+    mv unfixed_wheels/*.whl $WHEELHOUSE
+
 else
+
+    curl -OL https://raw.githubusercontent.com/matthew-brett/manylinux-builds/master/common_vars.sh
+
     DOCKER_IMAGE=quay.io/pypa/manylinux1_x86_64
     docker pull $DOCKER_IMAGE
     docker run --rm \
